78587956e98af7307849a43718a0e0ab8bb651ae
Nikolas-ComSci <kmalcala@up.edu.ph>
2023-05-03 18:36:27 +0800

    feat: accomplishd connection with firebase, complete with functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 /*
   Created by: Claizel Coubeili Cepe
   Date: updated April 26, 2023
@@ -9,7 +13,12 @@ import 'package:provider/provider.dart';
 import 'providers/todo_provider.dart';
 import 'screens/todo_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MultiProvider(
       providers: [
